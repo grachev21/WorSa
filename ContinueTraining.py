f413001f4c2f4186609f0ff42c3a02991dd7c1b3
grachev21 <grachev613@gmail.com>
2024-01-16 23:07:30 +0400

    status_02

--- ContinueTraining.py
+++ ContinueTraining.py
@@ -14,30 +14,39 @@ class ContinueTraining:
                 return json.load(file)
 
     def count_words(self):
+        word_list = []
+        number = 0
 
         if not os.path.isfile('./file_json/gussed_words.json'):
-            self.record_read('gussed_words', None, 'record')
+            self.record_read('gussed_words', [], 'record')
+        
+        if not os.path.isfile('./fiile_json/count_words.json'):
+            self.record_read('count_words', [], 'record')
 
+        # Получаем накопительный список
         gussed_words = self.record_read('gussed_words', None, 'read')
+
         # Получаем настройки
         number_word = self.record_read('settings', None, 'read')['number_word']
-
 
         # Получаем все слова
         my_dict = self.record_read('my_dict', None, 'read')
 
-        word_list = []
-        number = 0
-        while len(word_list) < number_word:
-            
-            if my_dict[number] not in 'none':
-                word_list.append(my_dict[number])
-            print('x')
+
+        while len(word_list) <= number_word:
+            if my_dict[number] not in gussed_words:
+                append_dict = my_dict[number]
+                append_dict['status'] = 0
+                word_list.append(append_dict)
             number += 1
-        #     
-        #
-        #
-        #
+        print(word_list)
+        
+        return self.record_read('count_words', word_list, 'record')
+            
+        
+        
+        
+        
         # if not os.path.isfile('./file_json/count_words.json'):
         #     with open('./file_json/count_words.json', 'w') as f:
         #         json.dump(word_list, f, sort_keys=True, indent=2, ensure_ascii=False)
